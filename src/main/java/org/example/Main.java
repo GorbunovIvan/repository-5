9bbb4bec932599a52c295432aff364960a3ee365
Ivan Gorbunov <gorbunovvivan@gmail.com>
2022-09-03 14:19:55 +0300

    new methods from nextBranch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    
+    public static void method4() {
+        System.out.println("method-3");
+    }
+    
+    public static void method5() {
+        System.out.println("method-4");
+    }
 }
